43a5a2a64df4dae7aeb20c409776e635523aec3c
Elie Asmar <elieasmar89@gmail.com>
2023-09-19 15:37:07 +0200

    solution

---
+++ README.md
@@ -0,0 +1,51 @@
+# Looping festival .. or ?
+
+This exercise can turn into a looping festival if we don't pay attention to performance!
+The suggested data structure helps reduce loops by providing an O(1) access time when marking numbers.
+The below is an example of how one board is represented:
+
+```txt
+'0': {
+    numbers: {
+    '5': [ 2, 0, false ],
+    '6': [ 2, 4, false ],
+    '10': [ 1, 2, false ],
+    '12': [ 1, 3, false ],
+    '23': [ 0, 1, false ],
+    '25': [ 2, 3, false ],
+    '26': [ 1, 4, false ],
+    '27': [ 1, 0, false ],
+    '29': [ 3, 3, false ],
+    '32': [ 4, 3, false ],
+    '33': [ 4, 0, false ],
+    '34': [ 2, 1, false ],
+    '40': [ 3, 1, false ],
+    '41': [ 4, 2, false ],
+    '53': [ 1, 1, false ],
+    '54': [ 3, 4, false ],
+    '56': [ 3, 0, false ],
+    '65': [ 0, 2, false ],
+    '68': [ 4, 1, false ],
+    '73': [ 3, 2, false ],
+    '78': [ 0, 3, false ],
+    '82': [ 4, 4, false ],
+    '83': [ 2, 2, false ],
+    '85': [ 0, 0, false ],
+    '93': [ 0, 4, false ]
+  },
+    rowsCounters: [ 0, 0, 0, 0, 0 ],
+    colsCounters: [ 0, 0, 0, 0, 0 ],
+    winner: false
+  },
+```
+
+This an example of the first board:
+
+- board number is 0
+- board numbers are keys
+- each key has an array as its value and this array contains the following info:
+  - which row the number belongs to
+  - which col the number belongs to
+  - whether the number is marked or not (initialized to `false`)
+- `rowsCounters` and `colsCounters` are updated whenever a value is marked and once an element reaches 5, we know the board is a winner.
+- `winner` stating if the board is a winner or not yet, also initialized to `false` and turned into `true` once it wins so we avoid checking it in the future to also help perfomance.
--- index.js
+++ index.js
@@ -1,40 +1,84 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const input = fs.readFileSync('testData.txt', 'utf8');
-const lines = input.split('\n');
-const drawnNumbers = lines[0].split(',').map(Number);
+const input = fs.readFileSync("realData.txt", "utf8");
+const lines = input.split("\n").filter(Boolean);
+const drawnNumbers = lines[0].split(",").map(Number);
 
 let boards = {};
 let boardIndex = 0;
+let winningBoards = [];
 
-for (let i = 1; i < lines.length; i += 5) {
-  const boardRows = lines
-    .slice(i, i + 5)
-    .map((line) => line.split(' ').map(Number));
+function extractData() {
+  for (let i = 1; i < lines.length; i += 5) {
+    const boardRows = lines
+      .slice(i, i + 5)
+      .map((line) => line.split(" ").filter(Boolean).map(Number));
 
-  boards[boardIndex] = {
-    rows: {},
-    cols: {},
-    rowsCounters: Array(5).fill(0),
-    colsCounters: Array(5).fill(0),
-  };
+    boards[boardIndex] = {
+      numbers: {},
+      rowsCounters: Array(5).fill(0),
+      colsCounters: Array(5).fill(0),
+      winner: false,
+    };
 
-  for (let row = 0; row < boardRows.length; row++) {
-    for (let col = 0; col < boardRows[row].length; col++) {
-      const number = boardRows[row][col];
-      boards[boardIndex].rows[number] = [row, false];
-      boards[boardIndex].cols[number] = [col, false];
+    for (let row = 0; row < boardRows.length; row++) {
+      for (let col = 0; col < boardRows[row].length; col++) {
+        const number = boardRows[row][col];
+        boards[boardIndex].numbers[number] = [row, col, false];
+      }
+    }
+
+    boardIndex++;
+  }
+}
+
+function main() {
+  for (let i = 0; i < drawnNumbers.length; i++) {
+    let num = drawnNumbers[i];
+    for (let boardKey in boards) {
+      let board = boards[boardKey];
+
+      if (board.winner) {
+        continue;
+      }
+
+      if (board.numbers[num]) {
+        let rowIndex = board.numbers[num][0];
+        let colIndex = board.numbers[num][1];
+        board.numbers[num][2] = true;
+        board.rowsCounters[rowIndex]++;
+        board.colsCounters[colIndex]++;
+
+        if (checkForWinner(board)) {
+          winningBoards.push(boardKey);
+          if (winningBoards.length === Object.keys(boards).length) {
+            console.log("Last winner score:", calculateScore(boardKey) * num);
+            return;
+          }
+          board.winner = true;
+        }
+      }
+    }
+  }
+}
+
+function checkForWinner(board) {
+  return (
+    board.rowsCounters.some((val) => val === 5) ||
+    board.colsCounters.some((val) => val === 5)
+  );
+}
+
+function calculateScore(boardKey) {
+  let sum = 0;
+  for (let numberKey in boards[boardKey].numbers) {
+    if (!boards[boardKey].numbers[numberKey][2]) {
+      sum += +numberKey;
     }
   }
 
-  boardIndex++;
+  return sum;
 }
 
-//test to see if i'm sleepy
-if (boards[0].rows[3]) {
-  boards[0].rows[3][1] = true;
-  let row = boards[0].rows[3][0];
-  boards[0].rowsCounters[row]++;
-}
-
-console.log(boards[0]);
+extractData();
+main();
